d1c46f0732d60bdbf0ecec0477ea2a7c6162e046
Karim Dahmani <karim.dahmani88@gmail.com>
2023-12-29 15:10:24 +0100

    fix resetGallery+test+regroup edit functions

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -6,8 +6,7 @@ let worksCounter = 0;
 const filters = document.querySelector(".filters");
 const gallery = document.getElementById("gallery");
 const galleryModal = document.getElementById("galleryModal");
-const ban = document.querySelector("header");
-const loginButton = document.querySelector("[href='./login.html']");
+const loginButton = document.getElementById("loginButton");
 const token = window.sessionStorage.token;
 const modal = document.getElementById("modal");
 const closeButton = document.querySelector(".close");
@@ -27,13 +26,13 @@ const responseWorks = await fetch("http://localhost:5678/api/works");
 works = await responseWorks.json();
 worksCounter = works.length;
 
-/// FONCTIONS
-// Reset gallery
+/// FONCTIONS PRINCIPALES
+// Reset gallerie
 function resetGallery(zone) {
     zone.innerHTML = "";
 }
 // Récupération des noms des catégories et construction des boutons de filtres
-async function buildCategories(zone="home") {
+function buildCategories() {
         // ajout du bouton TOUS
         const allCatFilter = document.createElement("button");
         allCatFilter.setAttribute("type","button");
@@ -53,8 +52,11 @@ async function buildCategories(zone="home") {
             newFilter.innerText = categories[i].name;
         }
 }
-// Récupération des travaux et construction de la gallery
-async function buildGallery(id, zone) {
+// Récupération des travaux et construction de la gallerie
+function buildGallery(id, zone) {
+        // reset de la gallery
+        zone === "home" ? resetGallery(gallery) : resetGallery(galleryModal);
+        //ajout des projets
         for (let i = 0; i < worksCounter; i++) {
             if( works[i].categoryId === id || id === 0) {
                 const newWork = document.createElement("figure");
@@ -82,73 +84,63 @@ async function buildGallery(id, zone) {
             }
         }
 }
-// Filtrer
+// Fonction de filtration de la gallerie
 filters.addEventListener("click", function (event) {
     const activeFilter = document.getElementById("selectedFilter");
-    if (event.target.classList.contains("filter")) {
-        resetGallery(gallery);
+    const target = event.target;
+    if (target.classList.contains("filter")) {
         activeFilter.removeAttribute("id");
-        event.target.setAttribute("id","selectedFilter");
-        buildGallery(Number(event.target.getAttribute("data-categoryId")));       
+        target.setAttribute("id","selectedFilter");
+        buildGallery(Number(target.getAttribute("data-categoryId")), "home");       
     };
 });
-// Mode édition
+// Fonction de passage en mode édition
 function editModePage() {
-    if (!token) return;
-    else {
-        // Création de la bannière
-        const editModeBan = document.createElement("banner");
-        editModeBan.classList.add("editBan");
-        const editIcon = document.createElement("img");
-        editIcon.setAttribute("src", "./assets/icons/penWhite.png");
-        const banText = document.createElement("p");
-        banText.innerText = "Mode édition";
-        editModeBan.appendChild(editIcon);
-        editModeBan.appendChild(banText);
-        const body = document.querySelector("body");
-        body.insertBefore(editModeBan, body.childNodes[0]);
+    // Création de la bannière
+    const editModeBan = document.createElement("banner");
+    editModeBan.classList.add("editBan");
+    const editIcon = document.createElement("img");
+    editIcon.setAttribute("src", "./assets/icons/penWhite.png");
+    const banText = document.createElement("p");
+    banText.innerText = "Mode édition";
+    editModeBan.appendChild(editIcon);
+    editModeBan.appendChild(banText);
+    const body = document.querySelector("body");
+    body.insertBefore(editModeBan, body.childNodes[0]);
 
-        // Création du bouton
-        const editIconBlack = document.createElement("img");
-        editIconBlack.id = "blackPen";
-        editIconBlack.setAttribute("src", "./assets/icons/penBlack.png");
-        const manageButton = document.createElement("a");
-        manageButton.classList.add("editButton");
-        manageButton.setAttribute("href", "#modal");
-        manageButton.innerText = "modifier"
-        const projectTitle = document.getElementById("titleMenu");
-        projectTitle.appendChild(editIconBlack);
-        projectTitle.appendChild(manageButton);
-        manageButton.addEventListener("click", openModal);
+    // Création du bouton
+    const editIconBlack = document.createElement("img");
+    editIconBlack.id = "blackPen";
+    editIconBlack.setAttribute("src", "./assets/icons/penBlack.png");
+    const manageButton = document.createElement("a");
+    manageButton.classList.add("editButton");
+    manageButton.setAttribute("href", "#modal");
+    manageButton.innerText = "modifier"
+    const projectTitle = document.getElementById("titleMenu");
+    projectTitle.appendChild(editIconBlack);
+    projectTitle.appendChild(manageButton);
+    manageButton.addEventListener("click", openModal);
 
-        // Modification du bouton de LOGIN en LOGOUT
-        loginButton.innerText ="logout";
-        loginButton.setAttribute("href","#");
+    // Modification du bouton de LOGIN en LOGOUT
+    loginButton.innerText ="logout";
+    loginButton.setAttribute("href","#");
 
-        // Masquer les filtres
-        filters.classList.add("visibilityHidden");
+    // Masquer les filtres
+    filters.classList.add("visibilityHidden");
 
-        // LOGOUT - clean sessionStorage (FAIRE UNE FONCTION POUR LA REUTILISER DANS LE IF RETURN)
-        loginButton.addEventListener("click", function() {
-            window.sessionStorage.removeItem("token");
-            filters.classList.remove("visibilityHidden");
-            loginButton.innerText ="login";
-            loginButton.setAttribute("href","login.html");
-            manageButton.classList.add("visibilityHidden");
-            editModeBan.style.display = "none";
-        });
-    }
+    // LOGOUT - clean sessionStorage (FAIRE UNE FONCTION POUR LA REUTILISER DANS LE IF RETURN)
+    loginButton.addEventListener("click", function() {
+        window.sessionStorage.removeItem("token");
+        filters.classList.remove("visibilityHidden");
+        loginButton.innerText ="login";
+        loginButton.setAttribute("href","./login.html");
+        manageButton.classList.add("visibilityHidden");
+        editIconBlack.classList.add("visibilityHidden");
+        editModeBan.style.display = "none";
+    });
 };
 
-/// UTILISATION DES FONCTIONS
-// Initialisation
-resetGallery(gallery);
-buildCategories();
-buildGallery(0, "home");
-// Activation du mode édition
-editModePage();
-
-/// MODAL
+/// FONCTIONS DE GESTION DE LA MODAL
 // Ouverture, Fermeture, Navigation et Formulaire
 function createCatList() {
     for (let i = 0; i < categoriesCounter; i++) {
@@ -159,131 +151,140 @@ function createCatList() {
         optionList.appendChild(option);
     }
 }
-createCatList();
-
+// Ouverture de la modal
 function openModal(e) {
     e.preventDefault();
-    resetGallery(galleryModal);
     buildGallery(0, "modal");
     modal.style.display = "flex";
 }
-
+// Fermeture de la modal
 function closeModal(e) {
     e.preventDefault();
     step2.style.display = "none";
     step1.style.display = "flex";
     modal.style.display = "none";
 }
-
+// Navigation vers l'étape 2 de la modal
 function goToStep2(e) {
     e.preventDefault();
     step1.style.display = "none";
     step2.style.display = "flex";
 }
-
+// Navigation pour retour à l'étape 1 de la modal
 function backToStep1(e) {
     e.preventDefault();
     step2.style.display = "none";
     step1.style.display = "flex";
 }
 
-// Fermer la modal avec la touche ECHAP
-window.addEventListener('keydown', function(e) {
-    if (e.key === "Escape" || e.key === "Esc") {
-        closeModal(e);
-    }
-})
+/// UTILISATION DES FONCTIONS
+// Initialisation
+buildCategories();
+buildGallery(0, "home");
 
-// Fermer la modal en cliquant sur le layer
-modal.addEventListener("click", function(e) {
-    closeModal(e);
-})
+// Activation du mode édition
+if (token) {
+    editModePage();
+    createCatList();
 
-// Empêcher la fermeture de la modal si le click est dans la fenêtre modal
-const stopPropagation = function(e) {
-    e.stopPropagation();
-}
-
-document.querySelector(".modalWrapper").addEventListener("click", function(e) {
-    stopPropagation(e);
-})
-
-closeButton.addEventListener("click", closeModal);
-closeButton2.addEventListener("click", closeModal);
-addPicture.addEventListener("click", goToStep2);
-backArrow.addEventListener("click", backToStep1);
-
-//preview new upload image in form
-let newImgUpload = document.getElementById("submitPic");
-
-newImgUpload.onchange = evt => {
-    const [file] = newImgUpload.files;
-    if (file && file.size<32000000) {
-        previewUpload.src = URL.createObjectURL(file);
-        document.getElementById("newPic").style.display = "none";
-        document.querySelector(".customInput").style.display = "none";
-        document.getElementById("uploadInfo").style.display = "none";
-        document.getElementById("previewUpload").style.display = "flex";
-    }
-    else {
-        const uploadError = document.createElement("span");
-        uploadError.classList.add("uploadError");
-        document.querySelector(".addingBox").appendChild(uploadError);
-        uploadError.innerHTML = "Fichier trop volumineux.";
-    }
-}
-
-//authorize submit only when form is not empty
-document.getElementById("workCategory").onchange = evt => {
-    const title = document.getElementById("workTitle").value;
-    const category = document.getElementById("workCategory").value;
-    const submitButton = document.getElementById("uploadValidationForm");
-
-    if(title && category>0) {
-        submitButton.removeAttribute("id");
-        submitButton.classList.add("validPicture");
-    }
-    else if (category<1 || !title) {
-        alert("Veuillez renseigner les champs du formulaire");
-    }
-}
-
-// Ajouter du contenu
-function addWork() {
-    const id = Number(document.getElementById("workCategory").value);
-
-    const formData = new FormData();
-    formData.append("image", newImgUpload.files[0]);
-    formData.append("title", document.getElementById("workTitle").value);
-    formData.append("category", id);
-
-    fetch("http://localhost:5678/api/works", {
-        method: "POST",
-        headers: {
-            "Authorization":`Bearer ${token}`
-        },
-        body: formData,
+    // Fermer la modal avec la touche ECHAP
+    window.addEventListener('keydown', function(e) {
+        if (e.key === "Escape" || e.key === "Esc") {
+            closeModal(e);
+        }
     });
 
-    previewUpload.src = "";
+    // Fermer la modal en cliquant sur le layer
+    modal.addEventListener("click", function(e) {
+        closeModal(e);
+    });
 
-    // reset form
-    document.getElementById("newPic").removeAttribute("style");
-    document.querySelector(".customInput").removeAttribute("style");
-    document.getElementById("uploadInfo").removeAttribute("style");
-    document.getElementById("previewUpload").style.display ="none";
-    document.querySelector(".validPicture").setAttribute("id", "uploadValidationForm");
-    document.querySelector(".validPicture").classList.remove("validPicture");
-    document.getElementById("workTitle").value = "";
-    document.getElementById("workCategory").value = "";
+    // Empêcher la fermeture de la modal si le click est dans la fenêtre modal
+    const stopPropagation = function(e) {
+        e.stopPropagation();
+    };
 
-    step2.style.display = "none";
-    step1.style.display = "flex";
-    modal.style.display = "none";
+    document.querySelector(".modalWrapper").addEventListener("click", function(e) {
+        stopPropagation(e);
+    });
 
-};
+    closeButton.addEventListener("click", closeModal);
+    closeButton2.addEventListener("click", closeModal);
+    addPicture.addEventListener("click", goToStep2);
+    backArrow.addEventListener("click", backToStep1);
 
-document.getElementById("uploadValidationForm").addEventListener("click", addWork);
+    //preview new upload image in form
+    let newImgUpload = document.getElementById("submitPic");
+
+    newImgUpload.onchange = evt => {
+        const [file] = newImgUpload.files;
+        if (file && file.size<32000000) {
+            previewUpload.src = URL.createObjectURL(file);
+            document.getElementById("newPic").style.display = "none";
+            document.querySelector(".customInput").style.display = "none";
+            document.getElementById("uploadInfo").style.display = "none";
+            document.getElementById("previewUpload").style.display = "flex";
+        }
+        else {
+            const uploadError = document.createElement("span");
+            uploadError.classList.add("uploadError");
+            document.querySelector(".addingBox").appendChild(uploadError);
+            uploadError.innerHTML = "Fichier trop volumineux.";
+        }
+    };
+
+    // Authoriser l'envoi du formulaire si il est bien rempli
+    document.getElementById("workCategory").onchange = evt => {
+        const title = document.getElementById("workTitle").value;
+        const category = document.getElementById("workCategory").value;
+        const submitButton = document.getElementById("uploadValidationForm");
+
+        if(title && category>0) {
+            submitButton.removeAttribute("id");
+            submitButton.classList.add("validPicture");
+        }
+        else if (category<1 || !title) {
+            alert("Veuillez renseigner les champs du formulaire");
+        }
+    };
+
+    // Ajouter du contenu
+    function addWork() {
+        const id = Number(document.getElementById("workCategory").value);
+
+        const formData = new FormData();
+        formData.append("image", newImgUpload.files[0]);
+        formData.append("title", document.getElementById("workTitle").value);
+        formData.append("category", id);
+
+        fetch("http://localhost:5678/api/works", {
+            method: "POST",
+            headers: {
+                "Authorization":`Bearer ${token}`
+            },
+            body: formData,
+        });
+
+        previewUpload.src = "";
+
+        // reset form
+        document.getElementById("newPic").removeAttribute("style");
+        document.querySelector(".customInput").removeAttribute("style");
+        document.getElementById("uploadInfo").removeAttribute("style");
+        document.getElementById("previewUpload").style.display ="none";
+        document.querySelector(".validPicture").setAttribute("id", "uploadValidationForm");
+        document.querySelector(".validPicture").classList.remove("validPicture");
+        document.getElementById("workTitle").value = "";
+        document.getElementById("workCategory").value = "";
+
+        step2.style.display = "none";
+        step1.style.display = "flex";
+        modal.style.display = "none";
+
+    };
+
+    document.getElementById("uploadValidationForm").addEventListener("click", addWork);
+}
 
 // Supprimer du contenu
 function deleteWork(id, token) {
@@ -294,4 +295,4 @@ function deleteWork(id, token) {
             "Authorization":`Bearer ${token}`
         },
     });
-}
+};
